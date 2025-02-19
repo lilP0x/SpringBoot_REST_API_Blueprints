5ff5d981bd8248347a727e8ec88ab9947abc74f1
Mar972310 <maria.torres-m@mail.escuelaing.edu.co>
2025-02-19 17:44:01 -0500

    Pruebas check pero desde la persistencia falta del service

--- src/test/java/edu/eci/arsw/blueprints/test/persistence/impl/Impl/InMemoryPersistenceTest.java
+++ src/test/java/edu/eci/arsw/blueprints/test/persistence/impl/Impl/InMemoryPersistenceTest.java
@@ -3,19 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.eci.arsw.blueprints.test.persistence.impl;
+package edu.eci.arsw.blueprints.test.persistence.impl.Impl;
 
 import edu.eci.arsw.blueprints.model.Blueprint;
 import edu.eci.arsw.blueprints.model.Point;
 import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
 import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
 import edu.eci.arsw.blueprints.persistence.impl.InMemoryBlueprintPersistence;
-import edu.eci.arsw.blueprints.services.BlueprintsServices;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -65,29 +64,71 @@ public class InMemoryPersistenceTest {
             fail("An exception was expected after saving a second blueprint with the same name and autor");
         }
         catch (BlueprintPersistenceException ex){
-            
-        }
-                
-        
+
+        }        
     }
 
     @Test
-    public void getBlueprint() {
+    public void getBlueprintTest(){
         InMemoryBlueprintPersistence ibpp=new InMemoryBlueprintPersistence();
-        BlueprintsServices bpp = new BlueprintsServices();
-        
+
         Point[] pts=new Point[]{new Point(0, 0),new Point(10, 10)};
         Blueprint bp=new Blueprint("john", "thepaint",pts);
 
         try {
-            Blueprint blueprint = bpp.getBlueprint("john", "thepaint");
-            assertEquals(blueprint, bp);
-        } catch (BlueprintNotFoundException ex) {
-            fail("Blueprint not found");
+            ibpp.saveBlueprint(bp);
+        } catch (BlueprintPersistenceException ex) {
+            fail("Blueprint persistence failed inserting the first blueprint.");
         }
-                
-        
+
+        try {
+            Blueprint blueprint = ibpp.getBlueprint("john","thepaint");
+            assertEquals(blueprint, bp);
+
+        } catch (BlueprintNotFoundException ex) {
+            fail("Blueprint persistence failed search the blueprint.");
+        }
     }
 
+    @Test
+    public void getBlueprintByAuthorTest() throws BlueprintPersistenceException {
+        InMemoryBlueprintPersistence ibpp = new InMemoryBlueprintPersistence();
     
+        Point[] pts1 = new Point[]{new Point(0, 0), new Point(10, 10)};
+        Blueprint bp1 = new Blueprint("john", "thepaint1", pts1);  
+    
+        Point[] pts2 = new Point[]{new Point(10, 10), new Point(20, 20)};
+        Blueprint bp2 = new Blueprint("john", "thepaint2", pts2);  
+    
+        ibpp.saveBlueprint(bp1);
+        ibpp.saveBlueprint(bp2);
+    
+        Set<Blueprint> expectedBlueprints = new HashSet<>();
+        expectedBlueprints.add(bp1);
+        expectedBlueprints.add(bp2);
+    
+        try {
+            Set<Blueprint> blueprintAuthor = ibpp.getBlueprintsByAuthor("john"); 
+            assertEquals(expectedBlueprints, blueprintAuthor);
+        } catch (BlueprintNotFoundException ex) {
+            fail("No blueprints found for author: john");
+        }
+    }
+
+    @Test
+    public void getBlueprintsByNonExistentAuthorTest() throws BlueprintPersistenceException {
+    
+        InMemoryBlueprintPersistence ibpp = new InMemoryBlueprintPersistence();
+    
+        Point[] pts1 = new Point[]{new Point(0, 0), new Point(10, 10)};
+        Blueprint bp1 = new Blueprint("john", "thepaint1", pts1); 
+
+        ibpp.saveBlueprint(bp1);
+       
+        try {
+            Set<Blueprint> blueprintAuthor = ibpp.getBlueprintsByAuthor("Maria"); 
+        } catch (BlueprintNotFoundException ex) {
+            assertEquals("No blueprints found for author: Maria", ex.getMessage());;
+        }
+    }
 }
